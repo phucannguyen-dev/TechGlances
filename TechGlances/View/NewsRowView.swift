ef928f29b0a1a37aa983a0a2d6bbf6295214bcf7
An Nguyen <82162492+phucannguyen-dev@users.noreply.github.com>
2025-01-31 20:36:40 +0700

    Update NewsRowView.swift

--- TechGlances/View/NewsRowView.swift
+++ TechGlances/View/NewsRowView.swift
@@ -6,21 +6,44 @@ struct NewsRowView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text(news.title)
-                .font(.headline)
-                .onTapGesture {
-                    viewModel.openLink(news.link) // ✅ Fix: Call openLink from ViewModel
+            HStack(alignment: .lastTextBaseline) {
+                AsyncImage(url: news.image) { phase in
+                    switch phase {
+                    case .empty:
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.gray)
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                    @unknown default:
+                        EmptyView()
+                    }
                 }
-
-            if let summary = news.summary {
-                Text(summary)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-
-            Button("Summarize") {
-                // Implement summarization logic
-                
+                VStack(alignment: .leading) {
+                    Text(news.title)
+                        .font(.headline)
+                        .onTapGesture {
+                            viewModel.openLink(news.link) // ✅ Fix: Call openLink from ViewModel
+                        }
+                    Text(news.publicationDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                if let summary = news.summary {
+                    Text(summary)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                Button() {
+                    
+                } label: {
+                    Image(systemName: "text.append")
+                }
             }
         }
     }
